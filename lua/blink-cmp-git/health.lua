f840e30bbf708199f6bf7c6ec2a7ffd53fe467e6
Kaiser-Yang <624626089@qq.com>
2025-02-11 13:24:29 +0800

    fix: fix health.lua

--- lua/blink-cmp-git/health.lua
+++ lua/blink-cmp-git/health.lua
@@ -7,11 +7,11 @@ local utils = require('blink-cmp-git.utils')
 --- @return boolean
 local function check_command_executable(command, message)
     message = message or ('"' .. command .. '" not found.')
-    if utils.command_found(command) then
+    if not utils.command_found(command) then
         health.warn(message)
         return false
     end
-    health.ok(message)
+    health.ok('"' .. command .. '" found.')
     return true
 end
 
@@ -20,9 +20,9 @@ function M.check()
     check_command_executable('git')
     local should_check_curl = false
     should_check_curl = should_check_curl or
-        not check_command_executable('gh', 'gh not found, will use curl instead.')
+        not check_command_executable('gh', '"gh" not found, will use curl instead.')
     should_check_curl = should_check_curl or
-        not check_command_executable('glab', 'glab not found, will use curl instead.')
+        not check_command_executable('glab', '"glab" not found, will use curl instead.')
     if should_check_curl then
         check_command_executable('curl')
     end
