c179ea589ae70c7591d65dcf98e208415f617eef
Kaiser-Yang <624626089@qq.com>
2025-02-11 14:31:39 +0800

    feat: github issue closed by support

--- doc/item/github/issue.lua
+++ doc/item/github/issue.lua
@@ -1,46 +1,46 @@
 -- all empty strings in the table will be set to nil
 return {
-    url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/30",
+    url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/27",
     repository_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git",
-    labels_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/30/labels{/name}",
-    comments_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/30/comments",
-    events_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/30/events",
-    html_url = "https://github.com/Kaiser-Yang/blink-cmp-git/issues/30",
-    id = 2843947455,
-    node_id = "I_kwDONn0LzM6pgzG_",
-    number = 30,
-    title = "feature request: notify when reloading cache",
+    labels_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/27/labels{/name}",
+    comments_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/27/comments",
+    events_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/27/events",
+    html_url = "https://github.com/Kaiser-Yang/blink-cmp-git/issues/27",
+    id = 2840795137,
+    node_id = "I_kwDONn0LzM6pUxgB",
+    number = 27,
+    title = "[Feature Request]: close issue as `not planned` support and states for pr and issues",
     user = {
-        login = "austinliuigi",
-        id = 85013922,
-        node_id = "MDQ6VXNlcjg1MDEzOTIy",
-        avatar_url = "https://avatars.githubusercontent.com/u/85013922?v=4",
+        login = "MuntasirSZN",
+        id = 161931072,
+        node_id = "U_kgDOCabfQA",
+        avatar_url = "https://avatars.githubusercontent.com/u/161931072?v=4",
         gravatar_id = nil,
-        url = "https://api.github.com/users/austinliuigi",
-        html_url = "https://github.com/austinliuigi",
-        followers_url = "https://api.github.com/users/austinliuigi/followers",
-        following_url = "https://api.github.com/users/austinliuigi/following{/other_user}",
-        gists_url = "https://api.github.com/users/austinliuigi/gists{/gist_id}",
-        starred_url = "https://api.github.com/users/austinliuigi/starred{/owner}{/repo}",
-        subscriptions_url = "https://api.github.com/users/austinliuigi/subscriptions",
-        organizations_url = "https://api.github.com/users/austinliuigi/orgs",
-        repos_url = "https://api.github.com/users/austinliuigi/repos",
-        events_url = "https://api.github.com/users/austinliuigi/events{/privacy}",
-        received_events_url = "https://api.github.com/users/austinliuigi/received_events",
+        url = "https://api.github.com/users/MuntasirSZN",
+        html_url = "https://github.com/MuntasirSZN",
+        followers_url = "https://api.github.com/users/MuntasirSZN/followers",
+        following_url = "https://api.github.com/users/MuntasirSZN/following{/other_user}",
+        gists_url = "https://api.github.com/users/MuntasirSZN/gists{/gist_id}",
+        starred_url = "https://api.github.com/users/MuntasirSZN/starred{/owner}{/repo}",
+        subscriptions_url = "https://api.github.com/users/MuntasirSZN/subscriptions",
+        organizations_url = "https://api.github.com/users/MuntasirSZN/orgs",
+        repos_url = "https://api.github.com/users/MuntasirSZN/repos",
+        events_url = "https://api.github.com/users/MuntasirSZN/events{/privacy}",
+        received_events_url = "https://api.github.com/users/MuntasirSZN/received_events",
         type = "User",
         user_view_type = "public",
         site_admin = false
     },
     labels = {},
-    state = "open",
+    state = "closed",
     locked = false,
     assignee = nil,
     assignees = {},
     milestone = nil,
-    comments = 1,
-    created_at = "2025-02-10T23:28:43Z",
-    updated_at = "2025-02-11T01:26:34Z",
-    closed_at = nil,
+    comments = 29,
+    created_at = "2025-02-09T16:34:55Z",
+    updated_at = "2025-02-10T13:01:29Z",
+    closed_at = "2025-02-10T13:01:28Z",
     author_association = "NONE",
     sub_issues_summary = {
         total = 0,
@@ -48,23 +48,30 @@ return {
         percent_completed = 0
     },
     active_lock_reason = nil,
-    body = [[
-It would be nice if the user can get notified when a cache reload occurs. To do this, in the beginning of the `BlinkCmpGitReloadCache` callback, we can either:
-
-1. Add `vim.notify("BlinkCmpGit: reloading cache")`
-2. Fire a custom `BlinkCmpGitReloadPre` user autocommand and allow the user to add their own hook
-
-https://github.com/Kaiser-Yang/blink-cmp-git/blob/0a0fc76045799bd9d46aaf0a0acffad3edbf7966/lua/blink-cmp-git/init.lua#L173-L182
-
-On another note, I noticed that the autocommand that gets created is using the `BufEnter` event, but wouldn't it make more sense to use `DirChanged`?
-
-https://github.com/Kaiser-Yang/blink-cmp-git/blob/0a0fc76045799bd9d46aaf0a0acffad3edbf7966/lua/blink-cmp-git/init.lua#L183-L192
-
-Thanks for the great plugin :)
-]],
-    closed_by = nil,
+    body = "title ;) Currently, there is open and closed. Can we do open, closed as completed and closed as not planned?",
+    closed_by = {
+        login = "Kaiser-Yang",
+        id = 58209855,
+        node_id = "MDQ6VXNlcjU4MjA5ODU1",
+        avatar_url = "https://avatars.githubusercontent.com/u/58209855?v=4",
+        gravatar_id = nil,
+        url = "https://api.github.com/users/Kaiser-Yang",
+        html_url = "https://github.com/Kaiser-Yang",
+        followers_url = "https://api.github.com/users/Kaiser-Yang/followers",
+        following_url = "https://api.github.com/users/Kaiser-Yang/following{/other_user}",
+        gists_url = "https://api.github.com/users/Kaiser-Yang/gists{/gist_id}",
+        starred_url = "https://api.github.com/users/Kaiser-Yang/starred{/owner}{/repo}",
+        subscriptions_url = "https://api.github.com/users/Kaiser-Yang/subscriptions",
+        organizations_url = "https://api.github.com/users/Kaiser-Yang/orgs",
+        repos_url = "https://api.github.com/users/Kaiser-Yang/repos",
+        events_url = "https://api.github.com/users/Kaiser-Yang/events{/privacy}",
+        received_events_url = "https://api.github.com/users/Kaiser-Yang/received_events",
+        type = "User",
+        user_view_type = "public",
+        site_admin = false
+    },
     reactions = {
-        url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/30/reactions",
+        url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/27/reactions",
         total_count = 0,
         ["+1"] = 0,
         ["-1"] = 0,
@@ -75,7 +82,7 @@ Thanks for the great plugin :)
         rocket = 0,
         eyes = 0
     },
-    timeline_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/30/timeline",
+    timeline_url = "https://api.github.com/repos/Kaiser-Yang/blink-cmp-git/issues/27/timeline",
     performed_via_github_app = nil,
-    state_reason = nil
+    state_reason = "completed"
 }
