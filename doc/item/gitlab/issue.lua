b8eb30cc4f45042b6a6ce7a63a0fab0e7731731e
Kaiser-Yang <624626089@qq.com>
2025-02-11 15:29:22 +0800

    refactor: use api for gitlab

---
+++ doc/item/gitlab/issue.lua
@@ -0,0 +1,125 @@
+-- all empty strings in the table will be set to nil
+return {
+    id = 161591198,
+    iid = 7758,
+    project_id = 34675721,
+    title = "New command to delete labels",
+    description = [[
+### Problem to solve
+
+Right now you can only crate or list labels, not delete them.
+
+### Proposal
+
+Add a new command to delete labels.
+
+### Links / references
+    ]],
+    state = "closed",
+    created_at = "2025-02-01T15:37:10.879Z",
+    updated_at = "2025-02-05T08:31:45.636Z",
+    closed_at = "2025-02-05T08:31:45.593Z",
+    closed_by = {
+        id = 3457201,
+        username = "viktomas",
+        name = "Tomas Vik",
+        state = "active",
+        locked = false,
+        avatar_url = "https://gitlab.com/uploads/-/system/user/avatar/3457201/avatar.png",
+        web_url = "https://gitlab.com/viktomas"
+    },
+    labels = {
+        "devops::create",
+        "group::code review",
+        "section::dev",
+        "type::feature"
+    },
+    milestone = {
+        id = 4599724,
+        iid = 108,
+        group_id = 9970,
+        title = "17.9",
+        description = nil,
+        state = "active",
+        created_at = "2024-05-24T19:18:50.640Z",
+        updated_at = "2024-05-24T19:18:50.640Z",
+        due_date = "2025-02-14",
+        start_date = "2025-01-11",
+        expired = false,
+        web_url = "https://gitlab.com/groups/gitlab-org/-/milestones/108"
+    },
+    assignees = {
+        {
+            id = 2090646,
+            username = "rndmh3ro",
+            name = "Sebastian Gumprich",
+            state = "active",
+            locked = false,
+            avatar_url = "https://gitlab.com/uploads/-/system/user/avatar/2090646/avatar.png",
+            web_url = "https://gitlab.com/rndmh3ro"
+        }
+    },
+    author = {
+        id = 2090646,
+        username = "rndmh3ro",
+        name = "Sebastian Gumprich",
+        state = "active",
+        locked = false,
+        avatar_url = "https://gitlab.com/uploads/-/system/user/avatar/2090646/avatar.png",
+        web_url = "https://gitlab.com/rndmh3ro"
+    },
+    type = "ISSUE",
+    assignee = {
+        id = 2090646,
+        username = "rndmh3ro",
+        name = "Sebastian Gumprich",
+        state = "active",
+        locked = false,
+        avatar_url = "https://gitlab.com/uploads/-/system/user/avatar/2090646/avatar.png",
+        web_url = "https://gitlab.com/rndmh3ro"
+    },
+    user_notes_count = 0,
+    merge_requests_count = 1,
+    upvotes = 0,
+    downvotes = 0,
+    due_date = nil,
+    confidential = false,
+    discussion_locked = nil,
+    issue_type = "issue",
+    web_url = "https://gitlab.com/gitlab-org/cli/-/issues/7758",
+    time_stats = {
+        time_estimate = 0,
+        total_time_spent = 0,
+        human_time_estimate = nil,
+        human_total_time_spent = nil
+    },
+    task_completion_status = {
+        count = 0,
+        completed_count = 0
+    },
+    weight = nil,
+    blocking_issues_count = 0,
+    has_tasks = true,
+    task_status = "0 of 0 checklist items completed",
+    _links = {
+        self = "https://gitlab.com/api/v4/projects/34675721/issues/7758",
+        notes = "https://gitlab.com/api/v4/projects/34675721/issues/7758/notes",
+        award_emoji = "https://gitlab.com/api/v4/projects/34675721/issues/7758/award_emoji",
+        project = "https://gitlab.com/api/v4/projects/34675721",
+        closed_as_duplicate_of = nil
+    },
+    references = {
+        short = "#7758",
+        relative = "#7758",
+        full = "gitlab-org/cli#7758"
+    },
+    severity = "UNKNOWN",
+    moved_to_id = nil,
+    imported = false,
+    imported_from = "none",
+    service_desk_reply_to = nil,
+    epic_iid = nil,
+    epic = nil,
+    iteration = nil,
+    health_status = nil
+}
