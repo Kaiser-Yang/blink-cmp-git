7785c258c40f4aebee901e065f4b16a1b31d9f9b
Kaiser-Yang <624626089@qq.com>
2025-02-11 15:29:22 +0800

    refactor: use api for gitlab

---
+++ doc/item/gitlab/pull_request.lua
@@ -0,0 +1,151 @@
+-- all empty strings in the table will be set to nil
+return {
+    id = 361160839,
+    iid = 1906,
+    project_id = 34675721,
+    title = "fix(docs): document GLAB_CHECK_UPDATE environment variable",
+    description = [[
+## Description
+
+Updates the table as to document the `GLAB_CHECK_UPDATE` environment variable
+
+* fix(docs): document GLAB_CHECK_UPDATE environment variable
+
+## Related Issues
+
+/
+
+## How has this been tested?
+
+Manually and via tests in prior MRs
+
+## Screenshots (if appropriate):
+
+### Types of changes
+<!--- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
+- [~] Bug fix (non-breaking change which fixes an issue)
+- [~] New feature (non-breaking change which adds functionality)
+- [~] Breaking change (fix or feature that would cause existing functionality to change)
+- [x] Documentation
+- [~] Chore (Related to CI or Packaging to platforms)
+- [~] Test gap
+    ]],
+    state = "opened",
+    created_at = "2025-02-10T13:52:34.320Z",
+    updated_at = "2025-02-10T18:39:06.818Z",
+    merged_by = nil,
+    merge_user = nil,
+    merged_at = nil,
+    closed_by = nil,
+    closed_at = nil,
+    target_branch = "main",
+    source_branch = "faleksic-docs-glab-check-update",
+    user_notes_count = 4,
+    upvotes = 0,
+    downvotes = 0,
+    author = {
+        id = 9938509,
+        username = "faleksic",
+        name = "Filip Aleksic",
+        state = "active",
+        locked = false,
+        avatar_url = "https://gitlab.com/uploads/-/system/user/avatar/9938509/avatar.png",
+        web_url = "https://gitlab.com/faleksic"
+    },
+    assignees = {
+        {
+            id = 9938509,
+            username = "faleksic",
+            name = "Filip Aleksic",
+            state = "active",
+            locked = false,
+            avatar_url = "https://gitlab.com/uploads/-/system/user/avatar/9938509/avatar.png",
+            web_url = "https://gitlab.com/faleksic"
+        }
+    },
+    assignee = {
+        id = 9938509,
+        username = "faleksic",
+        name = "Filip Aleksic",
+        state = "active",
+        locked = false,
+        avatar_url = "https://gitlab.com/uploads/-/system/user/avatar/9938509/avatar.png",
+        web_url = "https://gitlab.com/faleksic"
+    },
+    reviewers = {
+        {
+            id = 5133434,
+            username = "aqualls",
+            name = "Amy Qualls",
+            state = "active",
+            locked = false,
+            avatar_url = "https://gitlab.com/uploads/-/system/user/avatar/5133434/avatar.png",
+            web_url = "https://gitlab.com/aqualls"
+        }
+    },
+    source_project_id = 34675721,
+    target_project_id = 34675721,
+    labels = {
+        "Category:GitLab CLI",
+        "Support Team Contributions",
+        "Technical Writing",
+        "cli",
+        "devops::create",
+        "documentation",
+        "group::code review",
+        "maintenance::refactor",
+        "section::dev",
+        "type::maintenance"
+    },
+    draft = false,
+    imported = false,
+    imported_from = "none",
+    work_in_progress = false,
+    milestone = {
+        id = 4599724,
+        iid = 108,
+        group_id = 9970,
+        title = "17.9",
+        description = nil,
+        state = "active",
+        created_at = "2024-05-24T19:18:50.640Z",
+        updated_at = "2024-05-24T19:18:50.640Z",
+        due_date = "2025-02-14",
+        start_date = "2025-01-11",
+        expired = false,
+        web_url = "https://gitlab.com/groups/gitlab-org/-/milestones/108"
+    },
+    merge_when_pipeline_succeeds = false,
+    merge_status = "unchecked",
+    detailed_merge_status = "unchecked",
+    merge_after = nil,
+    sha = "a9173a2bd75187ec8f5cdec4f09aeb81b36b29da",
+    merge_commit_sha = nil,
+    squash_commit_sha = nil,
+    discussion_locked = nil,
+    should_remove_source_branch = nil,
+    force_remove_source_branch = true,
+    prepared_at = "2025-02-10T13:52:45.253Z",
+    reference = "!1906",
+    references = {
+        short = "!1906",
+        relative = "!1906",
+        full = "gitlab-org/cli!1906"
+    },
+    web_url = "https://gitlab.com/gitlab-org/cli/-/merge_requests/1906",
+    time_stats = {
+        time_estimate = 0,
+        total_time_spent = 0,
+        human_time_estimate = nil,
+        human_total_time_spent = nil
+    },
+    squash = true,
+    squash_on_merge = true,
+    task_completion_status = {
+        count = 1,
+        completed_count = 1
+    },
+    has_conflicts = false,
+    blocking_discussions_resolved = true,
+    approvals_before_merge = nil
+}
--- lua/blink-cmp-git/default/gitlab.lua
+++ lua/blink-cmp-git/default/gitlab.lua
@@ -57,20 +57,8 @@ local function default_gitlab_issue_get_insert_text(item)
     return utils.concat_when_all_true('#', item.iid, '')
 end
 
-local function default_gitlab_issue_get_documentation(item)
-    return
-        utils.concat_when_all_true('#', item.iid, ' ', item.title, '\n') ..
-        utils.concat_when_all_true('State: ', item.state, '\n') ..
-        utils.concat_when_all_true('Author: ', item.author.username, '') ..
-        utils.concat_when_all_true(' (', item.author.name, ')') .. '\n' ..
-        utils.concat_when_all_true('Created at: ', item.created_at, '\n') ..
-        utils.concat_when_all_true('Updated at: ', item.updated_at, '\n') ..
-        utils.concat_when_all_true('Closed  at: ', item.closed_at, '\n') ..
-        utils.concat_when_all_true(item.description, '')
-end
-
 local function default_gitlab_mr_get_label(item)
-    return utils.concat_when_all_true('!', item.iid, ' ', item.title, '')
+    return utils.concat_when_all_true('!', item.iid, ' ', item.title)
 end
 
 local function default_gitlab_mr_get_kind_name(_)
@@ -78,26 +66,33 @@ local function default_gitlab_mr_get_kind_name(_)
 end
 
 local function default_gitlab_mr_get_insert_text(item)
-    return utils.concat_when_all_true('!', item.iid, '')
+    return utils.concat_when_all_true('!', item.iid)
 end
 
-local function default_gitlab_mr_get_documentation(item)
+local function default_gitlab_mr_or_issue_get_documentation(item, is_mr)
     return
-        utils.concat_when_all_true('!', item.iid, ' ', item.title, '\n') ..
-        utils.concat_when_all_true('State: ', item.state, '\n') ..
+        utils.concat_when_all_true(is_mr and '!' or '#', item.iid, ' ', item.title, '\n') ..
+        utils.concat_when_all_true('State: ',
+            item.discussion_locked and 'locked' or
+            item.draft and 'draft' or
+            item.state, '\n') ..
         utils.concat_when_all_true('Author: ', item.author.username, '') ..
         utils.concat_when_all_true(' (', item.author.name, ')') .. '\n' ..
         utils.concat_when_all_true('Created at: ', item.created_at, '\n') ..
         utils.concat_when_all_true('Updated at: ', item.updated_at, '\n') ..
         (
-            item.state == 'MERGED' and
+            item.state == 'merged' and
             utils.concat_when_all_true('Merged  at: ', item.merged_at, '\n') ..
             utils.concat_when_all_true('Merged  by: ', item.merged_by.username, '') ..
             utils.concat_when_all_true(' (', item.merged_by.name, ')') .. '\n'
             or
-            utils.concat_when_all_true('Closed  at: ', item.closed_at, '\n')
+            item.state == 'closed' and
+            utils.concat_when_all_true('Closed  at: ', item.closed_at, '\n') ..
+            utils.concat_when_all_true('Closed  by: ', item.closed_by.username, '') ..
+            utils.concat_when_all_true(' (', item.closed_by.name, ')') .. '\n'
+            or ''
         ) ..
-        utils.concat_when_all_true(item.description, '')
+        utils.concat_when_all_true(item.description)
 end
 
 local function default_gitlab_mention_get_label(item)
@@ -112,13 +107,35 @@ local function default_gitlab_mention_get_insert_text(item)
     return utils.concat_when_all_true('@', item.username, '')
 end
 
+local function default_gitlab_get_command()
+    return utils.command_found('glab') and 'glab' or 'curl'
+end
+
+local function basic_args_for_gitlab_api(token)
+    if utils.truthy(token) then
+        return {
+            '-H', 'PRIVATE-TOKEN: ' .. token,
+        }
+    end
+    return {}
+end
+
 local function default_gitlab_mention_get_documentation(item)
     return {
-        get_command = 'glab',
-        get_command_args = {
-            'api',
-            'users/' .. tostring(item.id),
-        },
+        get_token = '',
+        get_command = default_gitlab_get_command,
+        get_command_args = function(command, token)
+            local args = basic_args_for_gitlab_api(token)
+            if command == 'curl' then
+                table.insert(args, '-s')
+                table.insert(args, '-f')
+                table.insert(args, 'https://gitlab.com/api/v4/users/' .. tostring(item.id))
+            else
+                table.insert(args, 1, 'api')
+                table.insert(args, 'users/' .. tostring(item.id))
+            end
+            return args
+        end,
         ---@diagnostic disable-next-line: redefined-local
         resolve_documentation = function(output)
             local user_info = utils.json_decode(output)
@@ -134,54 +151,77 @@ local function default_gitlab_mention_get_documentation(item)
     }
 end
 
+local function default_gitlab_mr_issue_mention_get_command_args(command, token, type_name)
+    local args = basic_args_for_gitlab_api(token)
+    if command == 'curl' then
+        table.insert(args, '-s')
+        table.insert(args, '-f')
+        table.insert(args,
+            'https://gitlab.com/api/v4/projects/' ..
+            utils.get_repo_owner_and_repo(true) .. '/' ..
+            type_name ..
+            ((type_name == 'merge_requests' or type_name == 'issues') and '?state=all' or '')
+        )
+    else
+        table.insert(args, 1, 'api')
+        table.insert(args,
+            'projects/' ..
+            utils.get_repo_owner_and_repo(true) .. '/' ..
+            type_name ..
+            ((type_name == 'merge_requests' or type_name == 'issues') and '?state=all' or '')
+        )
+    end
+    return args
+end
+
 --- @type blink-cmp-git.GCSOptions
 return {
     issue = {
         enable = default_gitlab_enable,
         triggers = { '#' },
-        get_command = 'glab',
-        get_command_args = {
-            'issue',
-            'list',
-            '--all',
-            '--output', 'json',
-        },
+        get_token = '',
+        get_command = default_gitlab_get_command,
+        get_command_args = function(command, token)
+            return default_gitlab_mr_issue_mention_get_command_args(command, token, 'issues')
+        end,
         insert_text_trailing = ' ',
         separate_output = common.json_array_separator,
         get_label = default_gitlab_issue_get_label,
         get_kind_name = default_gitlab_issue_get_kind_name,
         get_insert_text = default_gitlab_issue_get_insert_text,
-        get_documentation = default_gitlab_issue_get_documentation,
+        get_documentation = function(item)
+            return default_gitlab_mr_or_issue_get_documentation(item, false)
+        end,
         configure_score_offset = default_gitlab_mr_or_issue_configure_score_offset,
         on_error = common.default_on_error,
     },
     pull_request = {
         enable = default_gitlab_enable,
         triggers = { '!' },
-        get_command = 'glab',
-        get_command_args = {
-            'mr',
-            'list',
-            '--all',
-            '--output', 'json',
-        },
+        get_token = '',
+        get_command = default_gitlab_get_command,
+        get_command_args = function(command, token)
+            return default_gitlab_mr_issue_mention_get_command_args(command, token, 'merge_requests')
+        end,
         insert_text_trailing = ' ',
         separate_output = common.json_array_separator,
         get_label = default_gitlab_mr_get_label,
         get_kind_name = default_gitlab_mr_get_kind_name,
         get_insert_text = default_gitlab_mr_get_insert_text,
-        get_documentation = default_gitlab_mr_get_documentation,
+        get_documentation = function(item)
+            return default_gitlab_mr_or_issue_get_documentation(item, false)
+        end,
         configure_score_offset = default_gitlab_mr_or_issue_configure_score_offset,
         on_error = common.default_on_error,
     },
     mention = {
         enable = default_gitlab_enable,
         triggers = { '@' },
-        get_command = 'glab',
-        get_command_args = {
-            'api',
-            'projects/:id/users',
-        },
+        get_token = '',
+        get_command = default_gitlab_get_command,
+        get_command_args = function(command, token)
+            return default_gitlab_mr_issue_mention_get_command_args(command, token, 'users')
+        end,
         insert_text_trailing = ' ',
         separate_output = common.json_array_separator,
         get_label = default_gitlab_mention_get_label,
